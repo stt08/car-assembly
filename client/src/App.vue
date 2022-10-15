8e19da2649228745154bd63feecc0c5b70e73713
= <=>
2022-10-15 17:24:59 +0200

    create a vue app (client folder)

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <NavigationBar/>
+  <HelloWorld msg="Welcome"/>
+</template>
+
+<script>
+import NavigationBar from './components/NavigationBar.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+    HelloWorld,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ client/src/components/NavigationBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-md">
+      <a class="navbar-brand" href="/">Car Assembly</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="me-auto navbar-nav">
+          <a class="nav-link" href="storage">Storage</a>
+          <a class="nav-link" href="assembly">Assembly</a>
+          <a class="nav-link" href="products">Products</a>
+          <a class="nav-link" href="admin">Admin</a>
+        </div>
+        <button class="btn text-white" onclick="loginAction()">Login</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
