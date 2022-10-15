16bc875e5bb02b8f1bb9d05ec92f48300404663f
upolsk <79157689+upolsk@users.noreply.github.com>
2022-10-15 15:22:42 +0200

    created api for blueprints

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', require('./api/users.js'));
+app.use('/api/blueprints', require('./api/blueprints.js'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`[express]: Runs on port ${port}`));
